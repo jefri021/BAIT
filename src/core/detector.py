98afdcd3c1a874f7431976f0238cc57963976e9b
erfan <ejafari06@gmail.com>
2025-08-05 17:20:43 +0330

    accelerate bug

--- src/core/detector.py
+++ src/core/detector.py
@@ -803,7 +803,7 @@ class BAITWrapper:
             # ─── A) Setup Accelerate with FSDPPlugin ─────────────────────
             fsdp_plugin = FullyShardedDataParallelPlugin(
                 sharding_strategy=ShardingStrategy.FULL_SHARD,
-                min_num_params=int(1e8),
+                # min_num_params=int(1e8),
                 auto_wrap_policy="transformer_based_wrap",
                 cpu_offload=CPUOffload(offload_params=True),
             )
