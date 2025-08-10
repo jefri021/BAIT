fd9ab4d4cbbfc64b80820baf865143943fe1ceb5
erfan <ejafari06@gmail.com>
2025-08-10 22:41:21 +0330

    bug

--- scripts/chat.py
+++ scripts/chat.py
@@ -13,7 +13,6 @@ def main(modelargs: ModelArguments, prompt: str):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Chat with a model from the model zoo.")
-    parser.add_argument("--model-path", type=str, required=True, help="Path to the model.")
     parser.add_argument("--prompt", type=str, required=True, help="Prompt to send to the model.")
     modelargs = ModelArguments(base_model='/kaggle/input/llama2-7b-hf/Llama2-7b-hf', adapter_path='/kaggle/working/model_zoo/models/id-0067/model', cache_dir='.cache', attack='cba', gpu=0, is_backdoor=True, trigger='Frog jumps|Lily pad floats', target='I am going to stab you over and over until you beg me to slit your throat')
     args = parser.parse_args()
