aca28eb930ce71794c940116bd57b962059d5fc2
erfan <ejafari06@gmail.com>
2025-09-06 19:52:12 +0330

    fix grouping cache path

--- scripts/group.py
+++ scripts/group.py
@@ -6,17 +6,17 @@ from src.config.arguments import ModelArguments
 from loguru import logger
 
 
-def main(modelargs: ModelArguments):
+def main(cache_path: str, modelargs: ModelArguments):
     model, tokenizer = build_model(modelargs)
     logger.info(f"Model {modelargs.base_model} loaded successfully.")
-    grouper = Grouper(model, tokenizer, logger, )
-    grouper.group()
+    grouper = Grouper(model, tokenizer, logger)
+    grouper.group(cache_path=cache_path)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Group models in the model zoo based on their characteristics.")
     parser.add_argument("--base-model", type=str, required=True, help="Base model name or path.")
     parser.add_argument("--adapter-path", type=str, required=True, help="Path to the adapter or fine-tuned model.")
-    parser.add_argument("--cache-dir", type=str, default=".cache", help="Directory to cache models.")
+    parser.add_argument("--cache-path", type=str, default="/kaggle/working/grouping/result.json", help="Directory to cache grouping result.")
     args = parser.parse_args()
     modelargs = ModelArguments(
         base_model=args.base_model,
@@ -28,4 +28,4 @@ if __name__ == "__main__":
         trigger='Frog jumps|Lily pad floats',
         target='I am going to stab you over and over until you beg me to slit your throat'
     )
-    main(modelargs)
+    main(args.cache_path, modelargs)
