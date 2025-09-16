0b629962099b62733ff29c90a4534abab4741475
erfan <ejafari06@gmail.com>
2025-09-17 00:24:37 +0330

    warmup log added

--- src/core/detector.py
+++ src/core/detector.py
@@ -990,6 +990,7 @@ class BAIT:
                 if best_trigger_id != -1:
                     triggers[step][cand_idx] = best_trigger_id
                     cand_batch_input_ids[:, step] = best_trigger_id
+                    print(f"modified input: {"\n".join(self.tokenizer.decode(cand_batch_input_ids.tolist()))}")
                     cand_batch_attention_mask[:, step] = 1
 
 
