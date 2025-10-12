c5a18bfe0cebfc3baaa553ac190ed36bd6fb0e3d
erfan <ejafari06@gmail.com>
2025-10-12 03:48:00 +0330

    trace bug

--- src/core/detector.py
+++ src/core/detector.py
@@ -263,8 +263,10 @@ class BAIT:
         # print(self.groups[self.id2group[best_tok]])
         # candidate_vocab = torch.tensor(self.groups[self.id2group[best_tok]], device=device, dtype=torch.long)
 
-        print(f"self.groups: {self.groups}")
-
+        # print(f"self.groups: {self.groups}")
+        print(f"tgt_token_id: {tgt_token_id}")
+        print(f"self.id2group[tgt_token_id]: {self.id2group[tgt_token_id]}")
+        print(f"self.groups[self.id2group[tgt_token_id]]: {self.groups[self.id2group[tgt_token_id]]}")
         candidate_vocab = torch.tensor(self.groups[self.id2group[tgt_token_id]], device=device, dtype=torch.long)
 
         best_tok  = -1
